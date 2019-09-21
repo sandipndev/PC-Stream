d97d4008131090595c10a271b1745af0f5daecae
Sandipan Dey <sandipandey.12@gmail.com>
2019-09-21 23:40:57 +0530

    taylor swift

--- Server/index.js
+++ Server/index.js
@@ -7,12 +7,15 @@ const sqlite3 = require('sqlite3').verbose()
 var db = new sqlite3.Database('records.db')
 
 let mainWindow
+let serverState = 0
 
 // --- ELECTRON APP --
 app.on("ready", ()=> {
     mainWindow = new BrowserWindow({
         height: 800,
         width: 1200,
+        minHeight: 600,
+        minWidth: 1000,
         show: false,
         frame: false,
         webPreferences: {
@@ -34,4 +37,13 @@ app.on("ready", ()=> {
 app.on('window-all-closed', ()=>{ 
     app.quit()
     db.close()
+})
+
+ipcMain.on("toggle-server", () => {
+    if (serverState == 0) {
+        serverState = 1
+    } else {
+        serverState = 0
+    }
+    mainWindow.webContents.send("server-state", (serverState==1)?"on":"off")
 })
--- Server/views/app/main.js
+++ Server/views/app/main.js
@@ -8,11 +8,37 @@ document.getElementById('max-rest-btn').addEventListener('click', () => {
   const currentWindow = remote.getCurrentWindow()
   if(currentWindow.isMaximized()) {
     currentWindow.unmaximize()
+    document.getElementById('max-rest-btn').innerHTML = "&#128470;"
   } else {
     currentWindow.maximize()
+    document.getElementById('max-rest-btn').innerHTML = "&#128471;"
   }
 })
 
 document.getElementById('close-btn').addEventListener('click', () => {
   remote.app.quit()
+})
+
+document.getElementById('toggle-server-btn').addEventListener('click', () => {
+  console.log("b")
+  ipcRenderer.send('toggle-server')
+})
+
+ipcRenderer.on('server-state', (_, state) => {
+  console.log("a")
+  if (state === 'on') {
+    document.getElementById('status-text').innerText = "ONLINE"
+    document.getElementById('status-dot-1').style.color = "#17CC60" 
+    document.getElementById('status-dot-2').style.color = "#17CC60"
+    document.getElementById('toggle-server-btn').innerText = "Stop Server"
+    document.getElementById('toggle-server-btn').classList.remove("btn-dark")
+    document.getElementById('toggle-server-btn').classList.add("btn-success")
+  } else {
+    document.getElementById('status-text').innerText = "OFFLINE"
+    document.getElementById('status-dot-1').style.color = "#bbb"
+    document.getElementById('status-dot-2').style.color = "#bbb"
+    document.getElementById('toggle-server-btn').innerText = "Start Server"
+    document.getElementById('toggle-server-btn').classList.remove("btn-success")
+    document.getElementById('toggle-server-btn').classList.add("btn-dark")
+  }
 })
