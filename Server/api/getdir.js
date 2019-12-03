e359dd6f03d571a772e154dd90cbe6c8a0aa5d87
Sandipan Dey <sandipandey.12@gmail.com>
2019-12-03 17:01:24 +0530

    getdir done

--- Server/api/getdir.js
+++ Server/api/getdir.js
@@ -1,51 +1,61 @@
-const fs = require('fs')
-const path = require('path')
-const { resDataCheck, driveDataWin, isPathAbs, isDir, isFile } = require("../misc/randomfuncs")
+/* Imports for all apis */
+const { req_data_check, send_error } = require("../misc/randomfuncs")
+const path = require("path")
+const sqlite3 = require("sqlite3")
 
-const sqlite3 = require('sqlite3')
+/* To read a directory and generate drive data */
+const { driveDataWin, isPathAbs, isDir, isFile } = require("../misc/randomfuncs")
+const { readdir } = require("fs")
 
 module.exports = function ( req, res, emitter ) {
 
-    if(resDataCheck(req.body["dir"])) {
+    /* Username and Password are sent and of type Strings */
+    if( req_data_check(req.body["dir"]) ) {
+
+        /* Path given must be absolute */
+        if ( !isPathAbs(req.body["dir"]) ) {
+            res.status(400).send("PATH_NOT_ABS")
+            return
+        }
+
+        /* Requesting for root directory sends drive data in Windows */
+        if (req.body["dir"] == "/" && process.platform == "win32") {
+            driveDataWin().then((rx) => {
+                res.status(200).send({disks: rx})
+            })
+            return
+        }
         
-        // Username and Password are sent and of type Strings
+        /* Database Object */
+        let db = new sqlite3.Database(path.join(__dirname, "..", "records.db"))
 
-        // Database checks
-        var db = new sqlite3.Database(path.join(__dirname, '..', 'records.db'))
-        db.all(`SELECT folders_unallowed
-        FROM permissions
-        WHERE user_id = ?`, req.user_id, (_, r1)=> {
+        /* Get the not permitted folders */
+        db.all(`SELECT folders_unallowed FROM permissions WHERE user_id = ?`, req.user_id, 
+        (e, r) => {
 
-            let unallowed_dirs = JSON.parse(r1[0].folders_unallowed)
+            /* Database Error */
+            if (e) {
+                send_error(res, "DBERR", e)
+                return
+            }
 
-            if (process.platform == 'win32') {
+            /* Folders that are not allowed */
+            let unallowed_dirs = JSON.parse(r[0].folders_unallowed)
+
+            /* Windows, process dirs in "\\" way instead of "/" */
+            if (process.platform == "win32") {
                 for (var i=0; i<unallowed_dirs.length; i++) {
                     unallowed_dirs[i] = unallowed_dirs[i].replace(/\//g, "\\")
                 }
             }
 
-            if (req.body["dir"] == "/" && process.platform == "win32") {
-                driveDataWin().then((rx) => {
-                    res.status(200).send({disks: rx})
-                })
-                return
-            }
-
-            // only in win, there's a concept of drives so otherwise no drives will be asked
-
-            // Path must be absolute
-            if (!isPathAbs(req.body["dir"])) {
-                res.status(400).send("PATH_NOT_ABS")
-                return
-            }
-
-            // Given directory must not contain in unallowed
+            /* Given directory must not be an unallowed */
             if (unallowed_dirs.includes(req.body["dir"])) {
                 res.status(400).send("DIR_DNE")
                 return
             }
 
-            // Given directory is inside an unallowed dir
+            /* Neither must the directory be inside unallowed */
             for (var i=0; i<unallowed_dirs.length; i++) {
                 if (req.body["dir"].includes(unallowed_dirs[i])) {
                     res.status(400).send("DIR_DNE")
@@ -53,51 +63,76 @@ module.exports = function ( req, res, emitter ) {
                 }
             }
 
+            /* Create the basic response */
             var response = {}
             response["files"] = []
             response["folders"] = []
             response["others"] = []
 
-            fs.readdir(req.body["dir"], (err, files) => {
-                if (err) {
-                    res.status(500).send("SERVER_ERR")
-                } else {
+            /* Read the directory now */
+            readdir(req.body["dir"], (e, files) => {
 
+                /* There was some error while reading the directory */
+                if (e) {
+                    send_error(res, "FSERR", e)
+                    return
+                }
+                
+                else {
+                    /* Loop over the files */
                     for (var i=0; i<files.length; i++) {
 
+                        /* This element */
                         var element = files[i]
 
-                        // Full path
+                        /* Full path of current element */
                         var p = path.join(req.body["dir"], element)
 
                         try {
-                            // Seperating dirs and folders
+                            /* Seperating dirs and folders */
                             if (isDir(p)) {
+
+                                /* Current user doesn"t have the perms to see this dir */
                                 if (unallowed_dirs.includes(p))
                                     continue
+                                
+                                /* Add to folder */
                                 else
                                     response["folders"].push(element)
-                            } else if (isFile(p)) {
+                            } 
+                            
+                            else if (isFile(p)) {
+                                /* Add to file */
                                 response["files"].push(element)
                             }
+
+                            else {
+                                /* Add to others */
+                                response["others"].push(element)
+                            }
                         } catch {
-                            // Weird files causing errors
+                            /* Add to weird files causing errors */
                             response["others"].push(element)
                         }
-                        
                     }
 
+                    /* Send this data */
                     res.status(200).send(response)
 
+                    /* Raise event */
+                    emitter.send("api:getdir:ReqDir", {
+                        user_id: req.user_id,
+                        dir: req.body["dir"]
+                    })
                 }
             })
 
+            /* Closing database after database ops */
+            db.close()
         })
 
-        // Closing database after database ops
-        db.close()
     } else {
-        // Data not sent
+        /* Dir was not sent */
         res.status(400).send("DATA_X")
     }
 }
