0e87fca94513a15b8873052bf8a8bb766ede14e1
Sandipan Dey <sandipandey.12@gmail.com>
2019-12-03 17:38:02 +0530

    get picture done

--- Server/api/getPicture.js
+++ Server/api/getPicture.js
@@ -1,23 +1,31 @@
-const sqlite3 = require('sqlite3')
-const path = require('path')
+/* Imports for all apis */
+const { send_error } = require("../misc/randomfuncs")
+const path = require("path")
+const sqlite3 = require("sqlite3")
 
 module.exports = function ( req, res, emitter ) {
-    // DB checks
-    var db = new sqlite3.Database(path.join(__dirname, '..', 'records.db'))
+
+    /* Database Object */
+    let db = new sqlite3.Database(path.join(__dirname, "..", "records.db"))
+
+    /* Get name and profile picture from database */
     db.all(`SELECT user_pictures.profile_picture, account.real_name
     FROM user_pictures JOIN account ON user_pictures.user_id = account.user_id
-    WHERE account.user_id = ?`, req.user_id, (_, r1) => {
+    WHERE account.user_id = ?`, req.user_id, (e, r) => {
 
-        // Session Key does not exists
-        if (r1.length === 0) {
-            res.status(400).send("SKEY_X")
+        /* Database Error */
+        if (e) {
+            send_error(res, "DBERR", e)
             return
         }
 
+        /* Send the data */
         res.status(200).send({
-            base64DP: r1[0].profile_picture,
-            name: r1[0].real_name
+            base64DP: r[0].profile_picture,
+            name: r[0].real_name
         })
 
+        /* Closing database after database ops */
+        db.close()
     })
 }
