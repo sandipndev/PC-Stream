659af1b0b63cceaefcc4895ae6448044273ce392
Sandipan Dey <sandipandey.12@gmail.com>
2019-12-03 13:01:18 +0530

    authenticate perf

--- Server/api/authenticate.js
+++ Server/api/authenticate.js
@@ -1,58 +1,78 @@
-const sqlite3 = require('sqlite3')
+/* Imports for all apis */
+const { resDataCheck } = require("../misc/randomfuncs")
 const path = require('path')
+const sqlite3 = require('sqlite3')
+
+/* Hash for checking hashed password and we are using jwt auth */
 const { createHash } = require('crypto')
 const jwt = require('jsonwebtoken')
-const { resDataCheck } = require("../misc/randomfuncs")
 
 module.exports = function ( req, res, emitter, privateKey ) {
     
-    if(resDataCheck(req.body["username"]) && resDataCheck(req.body["password"])) {
+    if( resDataCheck(req.body["username"]) && resDataCheck(req.body["password"]) ) {
+        /* Username and Password are sent and of type Strings */
 
-        // Username and Password are sent and of type Strings
+        /* Database Object */
+        let db = new sqlite3.Database(path.join(__dirname, '..', 'records.db'))
 
-        // Database checks
-        var db = new sqlite3.Database(path.join(__dirname, '..', 'records.db'))
-        db.all(`SELECT user_id, salt, hashed_password FROM account WHERE user_name = ?`, req.body["username"], (_, r) => {
+        /* Confirming User ID and Password */
+        db.all(`SELECT account.user_id, account.salt, account.hashed_password, account.real_name, login_details.no_logins
+        FROM account JOIN login_details ON account.user_id = login_details.user_id
+        WHERE account.user_name = ?`, req.body["username"], (_, r) => {
             
-            // Check even if the user exists
+            /* Check even if the user exists */
             if (r.length > 0) {
 
-                // Recreate the sha256 hashed password using salt
+                /* Recreate the sha256 hashed password using salt */
                 const hshpw = createHash('sha256').update(req.body["password"] + r[0].salt, 'utf8').digest('hex')
 
-                // Password is correct
+                /* Password is correct */
                 if (hshpw === r[0].hashed_password) {
 
-                    db.all(`SELECT no_logins FROM login_details WHERE user_id = ?`, r[0].user_id, (_, n) => {
-                        db.run(`UPDATE login_details SET no_logins = ? WHERE user_id = ?`, [ n[0].no_logins+1, r[0].user_id ])
-                    })
+                    /* Update number of logins */
+                    db.run(`UPDATE login_details SET no_logins = ? WHERE user_id = ?`, [ r[0].no_logins+1, r[0].user_id ])
 
+                    /* Sign with jwt token */
                     jwt.sign({
                         user_id: r[0].user_id
                     },  
                         privateKey, 
                         { expiresIn: '30m' },
                         (_, token) => {
-                            res.status(200).send(token)
-                    })
 
+                            /* Send the correct response */
+                            res.status(200).send({
+                                jwt_token: token,
+                                no_logins: r[0].no_logins
+                            })
+
+                            /* Raise event for logged in user */
+                            emitter.send("api:authenticate:LoggedIn", {
+                                name: r[0].real_name,
+                                id: r[0].user_id
+                            })
+                        }
+                    )
                 }
 
-                // Incorrect Password
+                /* Incorrect Password */
                 else
                     res.status(403).send("PWORD_X")
             }
             
-            // Username not present in database
+            /* Username not present in database */
             else 
                 res.status(403).send("UNAME_X")
+
+            /* Close the database */
+            db.close()
+
         })
 
-        db.close()
-
-    } else {
-        // Incorrect data
-        res.status(400).send("DATA_X")
     }
 
+    /* Username and Password were not sent */
+    else 
+        res.status(400).send("DATA_X")
+    
 }
