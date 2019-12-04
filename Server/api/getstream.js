309f1fd6bf3242c5e7f4e33a7ab05af1f3040a02
Sandipan Dey <sandipandey.12@gmail.com>
2019-12-04 16:07:14 +0530

    get stream done

--- Server/api/getstream.js
+++ Server/api/getstream.js
@@ -1,45 +1,58 @@
-const { resDataCheck, pathExists, isFile, isPathAbs } = require("../misc/randomfuncs")
-const { randomBytes } = require('crypto')
-const path = require('path')
+/* Imports for all apis */
+const { req_data_check, send_error } = require("../misc/randomfuncs")
+const path = require("path")
+const sqlite3 = require("sqlite3")
 
-const sqlite3 = require('sqlite3')
+/* Create random token for stream */
+const { pathExists, isPathAbs } = require("../misc/randomfuncs")
+const { randomBytes } = require('crypto')
 
 module.exports = function ( req, res, emitter ) {
-    if (resDataCheck(req.body["file"])) {
 
-        // Database checks
-        var db = new sqlite3.Database(path.join(__dirname, '..', 'records.db'))
-        db.all(`SELECT folders_unallowed, can_download
-        FROM permissions
-        WHERE user_id = ?`, req.user_id, (_, r1)=> {
+    /* A file needs to be sent to stream */
+    if ( req_data_check(req.body["file"]) ) {
 
-            // Permission checking - Download perms required to stream
-            if (r1[0].can_download == 0) {
+        /* Path must be absolute */
+        if (!isPathAbs(req.body["file"])) {
+            res.status(400).send("PATH_NOT_ABS")
+            return
+        }
+
+        /* It is not a file */
+        if (!path.extname(req.body["file"])) {
+            res.status(400).send("FILE_X")
+        }
+
+        /* Database Object */
+        let db = new sqlite3.Database(path.join(__dirname, "..", "records.db"))
+
+        /* Get the not permitted folders */
+        db.all(`SELECT folders_unallowed, can_download FROM permissions WHERE user_id = ?`, req.user_id, 
+        (e, r)=> {
+
+            /* Database Error */
+            if (e) {
+                send_error(res, "DBERR", e)
+                return
+            }
+
+            /* Permission checking - Stream perms required */
+            if (r[0].can_download == 0) {
                 res.status(400).send("CANT_DL_STREAM")
                 return
             }
 
-            let unallowed_dirs = JSON.parse(r1[0].folders_unallowed)
+            /* Folders that are not allowed */
+            let unallowed_dirs = JSON.parse(r[0].folders_unallowed)
 
-            // correcting dir format for windows
-            if (process.platform == 'win32') {
+            /* Windows, process dirs in "\\" way instead of "/" */
+            if (process.platform == "win32") {
                 for (var i=0; i<unallowed_dirs.length; i++) {
                     unallowed_dirs[i] = unallowed_dirs[i].replace(/\//g, "\\")
                 }
             }
 
-            // Path must be absolute
-            if (!isPathAbs(req.body["file"])) {
-                res.status(400).send("PATH_NOT_ABS")
-                return
-            }
-
-            // Given is not a file
-            if (!isFile(req.body["file"])) {
-                res.status(400).send("FILE_DNE")
-            }
-
-            // Given file is inside an unallowed dir
+            /*  Given file is inside an unallowed dir*/
             for (var i=0; i<unallowed_dirs.length; i++) {
                 if (req.body["file"].includes(unallowed_dirs[i])) {
                     res.status(400).send("FILE_DNE")
@@ -47,40 +60,55 @@ module.exports = function ( req, res, emitter ) {
                 }
             }
 
-            // File has no existence really
+            /* File has really no existence */
             if (!pathExists(req.body["file"])) {
                 res.status(400).send("FILE_DNE")
                 return
             }
 
-            db.all(`SELECT percent_watched FROM watch_hist WHERE user_id = ? AND absolute_path = ?`, [
-                req.user_id,
-                req.body["file"]
-            ], (_, wTill) => {
+            /* Get last watched till, in case the user watched it previously */
+            db.all(`SELECT percent_watched FROM watch_hist WHERE user_id = ? AND absolute_path = ?`, [ req.user_id, req.body["file"] ], 
+            (e, watched_till) => {
 
-                // Generate Key
+                /* Database Error */
+                if (e) {
+                    send_error(res, "DBERR", e)
+                    return
+                }
+
+                /* Generate Key */
                 let key = randomBytes(20).toString('hex')
 
-                db.all(`INSERT INTO stream_keys(key, by_user, file) VALUES (?, ?, ?)`,[
-                    key,
-                    req.user_id,
-                    req.body["file"]
-                ], (err) => {
-                    if (!err)
-                        res.status(200).send({
-                            "watched_till": (wTill.length == 0) ? 0 : wTill[0].percent_watched,
-                            "token": key
-                        })
-                    else
-                        res.status(500).send("SERVER_ERR")
+                /* Insert key in database */
+                db.all(`INSERT INTO stream_keys(key, by_user, file) VALUES (?, ?, ?)`,[ key, req.user_id, req.body["file"] ], 
+                (e) => {
+                    /* Database Error */
+                    if (e) {
+                        send_error(res, "DBERR", e)
+                        return
+                    }
+
+                    /* Send response with stream token */
+                    res.status(200).send({
+                        "watched_till": (watched_till.length == 0) ? 0 : watched_till[0].percent_watched,
+                        "token": key
+                    })
+
+                    /* Emit event */
+                    emitter.emit("api:getstream:AskedStream", {
+                        user_id: req.user_id,
+                        file: req.body["file"]
+                    })
+
+                    /* Closing database after database ops */
+                    db.close()
                 })
-
             })
-
         })
-
-    } else {
-        // Data not sent
-        res.status(400).send("DATA_X")
     }
+    
+    /* File was not sent */
+    else
+        res.status(400).send("DATA_X")
+
 }
