3019c8df9cff4446e08215286d0fd8cca438dc13
Sandipan Dey <sandipandey.12@gmail.com>
2019-12-06 00:26:35 +0530

    Delete added

--- Server/api/deletex.js
+++ Server/api/deletex.js
@@ -1,44 +1,59 @@
-const { pathExists, isDir, isPathAbs, deleteFile, resDataCheck } = require("../misc/randomfuncs")
-const path = require('path')
-const sqlite3 = require('sqlite3')
+/* Imports for all apis */
+const { req_data_check, send_error } = require("../misc/randomfuncs")
+const path = require("path")
+const sqlite3 = require("sqlite3")
+
+const { pathExists,
+        isPathAbs, 
+        deleteFile } = require("../misc/randomfuncs")
 
 module.exports = function ( req, res, emitter ) {
-    if (resDataCheck(req.body["file"])) {
 
-        // Database checks
-        var db = new sqlite3.Database(path.join(__dirname, '..', 'records.db'))
-        db.all(`SELECT folders_unallowed, can_delete
-        FROM permissions
-        WHERE user_id = ?`, req.user_id, (_, r1)=> {
+    /* A file needs to be sent to be deleted */
+    if ( req_data_check(req.body["file"]) ) {
 
-            // Session Key exists and correct
-            let unallowed_dirs = JSON.parse(r1[0].folders_unallowed)
+        /*  Path must be absolute*/
+        if ( !isPathAbs(req.body["file"]) ) {
+            res.status(400).send("PATH_NOT_ABS")
+            return
+        }
 
-            // correcting dir format for windows
-            if (process.platform == 'win32') {
+        /* It is not a file */
+        if (!path.extname(req.body["file"])) {
+            res.status(400).send("FILE_X")
+            return
+        }
+
+        /* Database Object */
+        let db = new sqlite3.Database(path.join(__dirname, "..", "records.db"))
+
+        /* Get the not permitted folders */
+        db.all(`SELECT folders_unallowed, can_delete FROM permissions WHERE user_id = ?`, req.user_id, 
+        (e, r)=> {
+
+            /* Database Error */
+            if (e) {
+                send_error(res, "DBERR", e)
+                return
+            }
+
+            /* No perms */
+            if (r[0].can_delete === 0) {
+                res.status(400).send("CANT_DX")
+                return
+            }
+
+            /* Folders that are not allowed */
+            let unallowed_dirs = JSON.parse(r[0].folders_unallowed)
+
+            /* Windows, process dirs in "\\" way instead of "/" */
+            if (process.platform == "win32") {
                 for (var i=0; i<unallowed_dirs.length; i++) {
                     unallowed_dirs[i] = unallowed_dirs[i].replace(/\//g, "\\")
                 }
             }
 
-            // No perms
-            if (r1[0].can_delete === 0) {
-                res.status(400).send("CANT_DX")
-                return
-            }
-
-            // Path must be absolute
-            if (!isPathAbs(req.body["file"])) {
-                res.status(400).send("PATH_NOT_ABS")
-                return
-            }
-
-            if (isDir(req.body["file"])) {
-                res.status(400).send("DIR_CANT")
-                return
-            }
-
-            // Given file is inside an unallowed dir
+            /* Given file is inside an unallowed dir */
             for (var i=0; i<unallowed_dirs.length; i++) {
                 if (req.body["file"].includes(unallowed_dirs[i])) {
                     res.status(400).send("FILE_DNE")
@@ -46,24 +61,30 @@ module.exports = function ( req, res, emitter ) {
                 }
             }
 
-            // File has no existence really
-            if (!pathExists(req.body["file"])) {
+            /* File has really no existence */
+            if ( !pathExists(req.body["file"]) ) {
                 res.status(400).send("FILE_DNE")
                 return
             }
 
-            // Okay, now delete
-            deleteFile(req.body["file"], (err)=>{
-                if (err) {
-                    res.sendStatus(500)
+            /* Okay, now delete */
+            deleteFile(req.body["file"], (e)=>{
+
+                /* Error */
+                if (e) {
+                    /* This is error */
+                    send_error(res, "FSERR", e)
                 } else {
+                    /* Perfect, send ok */
                     res.sendStatus(200)
                 }
             })
 
         })
-    } else {
-        // Data not sent
+    } 
+
+    else {
+        /* File not sent */
         res.status(400).send("DATA_X")
     }
 }
